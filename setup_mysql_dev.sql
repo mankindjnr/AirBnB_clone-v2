ffd68788df8867133472ca8411ad0886ec0c4479
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-20 02:09:54 -0700

    script to prepare mysql server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,9 +2,8 @@
 -- create a database user password privileges and SELECT privilleges
 -- if DATABASE already exists the script won't fail
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- if the user already exists the script doesn't fail
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- the user should have all privileges on the databse and only that database
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- SELECT privileges to another database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
