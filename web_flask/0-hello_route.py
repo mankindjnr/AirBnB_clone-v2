bc9812308657cbde3ee767012fc68950f32d2766
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-04-20 16:58:35 +0000

    script that starts a Flask web application:

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""
+script that starts a Flask web application:
+"""
+import web_flask
 from flask import Flask
-
-#very basic flask application
 
 hello_route = Flask(__name__) #references this file
 
 # creating an index route to avoid a 404, in brackets is the url
 # the define the function for that route
-@app.route("/", strict_slashes=False)
+@hello_route.route("/", strict_slashes=False)
 def index():
-    return "hello, mankind!"
+    """
+    ROUTE  /: Hello HBNB!display
+    """
+    return "Hello, HBNB!"
 
 if __name__ == "__main__":
     hello_route.run(host='0.0.0.0', port=5000) # any errors will pop up in the web
