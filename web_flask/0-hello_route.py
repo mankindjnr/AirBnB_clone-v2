b450d5157bcab75df94792f6ad3931835b1d9bb5
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-04-21 00:28:53 -0700

    pycodestyle fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,7 +4,10 @@ script that starts a Flask web application:
 """
 from flask import Flask
 
-hello_route = Flask(__name__) #references this file
+
+# references this file
+hello_route = Flask(__name__)
+
 
 # creating an index route to avoid a 404, in brackets is the url
 # the define the function for that route
@@ -15,5 +18,6 @@ def index():
     """
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    hello_route.run(host='0.0.0.0', port=5000) # any errors will pop up in the web
+    hello_route.run(host='0.0.0.0', port=5000)
